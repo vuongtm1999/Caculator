cfad9d5c0d203b369c50bc4b8adcbb569039d425
vuongvitin <vuong@gmail.com>
2022-10-05 11:21:20 +0700

    Done

---
+++ src/components/Result.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Result({ result }) {
+    return ( 
+        <div className="result">
+            <p>{result}</p>
+        </div>
+     );
+}
+
+export default Result;
